f506c602ce85eb288b2d763ff6f665ca2d6da0c1
Raymond Tan <ray.tangent42@gmail.com>
2022-07-04 09:37:24 -0400

    working on intro.10.

--- Intro/10.fileNaming.ts
+++ Intro/10.fileNaming.ts
@@ -1,12 +1,18 @@
 function solution(names:string[]): string[]{
 	let table:{[key:string]:number} = {};
 	let renamed:string[] = [];
-	for(let name : names){
+	for(let name of names){
 		if(name in table){
 			const occur:number = table[name];
-			renamed.push(	
+			renamed.push(name + "(" + (occur+1) + ")"); 
+			table[name]++;
 		}else{
+			console.log(name);
+			renamed.push(name);
 			table[name] = 0;
 		}
 	}
+	return renamed;
 }
+
+console.log(solution(["doc", "doc", "image", "doc(1)", "doc"]));
