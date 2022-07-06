134b8271f369c987c4b298d3462ffad6fb087c48
Raymond Tan <ray.tangent42@gmail.com>
2022-07-06 18:59:31 -0400

    Done arrays.1

--- Interview/Arrays/1.firstDuplicate.ts
+++ Interview/Arrays/1.firstDuplicate.ts
@@ -1,7 +1,14 @@
 const solution = (a:number[]): number => {
 	let seen = new Set<number>();
-	
-	for(let x of a){
 		
+	for(let x of a){
+		if(seen.has(x)){
+			return x;
+		}
+		seen.add(x);
 	}	
+	return -1;
 }
+
+const a:number[] = [2, 1, 3, 5, 3, 2];
+console.log(solution(a));
